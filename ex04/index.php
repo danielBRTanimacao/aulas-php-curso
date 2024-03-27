5adc784dccda959a505f70d7c2c6fa24966f5e86
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-03-27 08:42:26 -0300

    tipos primitivos e constantes php

--- ex04/index.php
+++ ex04/index.php
@@ -9,7 +9,26 @@
     <h1>Teste</h1>
     <?php 
     
-        echo "novos Testes"
+        // $name = "Daniel";
+        // $sobrenome = "Tenório";
+        // const PAIS = "Brasil";
+        // $name = "Aparecida";
+        
+        // echo "Olá, $name $sobrenome, você mora em " . PAIS;
+        const ID = 1;
+        $name_person = "Daniel";
+        $salary = 0;
+        $age = 19;
+        $work = "Desenvolvedor";
+        $sex = "M";
+        $married = false;
+        if ($married) {
+            $married = "Sim";
+        } else {
+            $married = "Não";
+        }
+
+        echo "Olá $name_person </br> Sua idade é: $age </br> Seu sexo é: $sex </br> Você trabalha: $work </br> Você e casado? $married";
 
     ?>
 </body>
