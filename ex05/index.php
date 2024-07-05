b18f2c857c7ef32da376b2203635947fca18cf9f
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-07-05 18:44:58 -0300

    fazendo meus testes

---
+++ ex05/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>titulo</title>
+</head>
+<body>
+    <h1>Fazendo simples testes</h1>
+    <?php 
+        echo 'Testes'
+    ?>
+</body>
+</html>
