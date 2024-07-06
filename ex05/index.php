5136f781dfd49d4ec97a97ad0ee6a8963494234d
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-07-06 10:17:57 -0300

    tipos primitivos

--- ex05/index.php
+++ ex05/index.php
@@ -3,12 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>titulo</title>
+    <title>Tipos Primitivos</title>
 </head>
 <body>
-    <h1>Fazendo simples testes</h1>
-    <?php 
-        echo 'Testes'
+    <h2>Testes tipos primitivos</h2>
+    <?php
+        // 0x = hexadecimal 0b = binario 0 = Octal 
+        // $num = 010;
+        // echo "O valor da variavel e $num"
+
+        // $v = 300;
+        // // mostra o valor e o tipo
+        // var_dump($v);
+    
+        // $v = (int) 3e2;
+        // echo "o valor é $v";
+    
+        // $var = (int) "950";
+        // echo "<p>$var</p>";
+        // var_dump($var)
+
+        // $married = true;
+        
+        // $vetor = [
+        //     1, 2, 3, 4,
+        // ];
+
+        // // for ($i=0; $i < $vetor; $i++) { 
+            
+        // // }
+
+        class Person {
+            private string $name;
+        }
     ?>
 </body>
 </html>
