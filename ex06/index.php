fa87bbd85cd2ac5b9b12ec94c84c9fc49197d805
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-07-08 17:22:19 -0300

    manipulando strings

---
+++ ex06/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manipulando Strings</title>
+</head>
+<body>
+    <h1>Strings manipulando</h1>
+</body>
+</html>
